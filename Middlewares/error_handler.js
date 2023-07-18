69ac74aec27358d06911eb95f3d932c3b8aa1904
Abdullah <abdallah0115019@gmail.com>
2023-07-18 18:40:49 +0300

    handle errors

--- Middlewares/error_handler.js
+++ Middlewares/error_handler.js
@@ -1,7 +1,40 @@
 const error_handler = function (err, req, res, next) {
-    res.status(500).json({
-        message: 'Something went wrong',
-        status: 500
+    const cusomError = {
+        status: err.status || 500,
+        message: err.message || 'Something went wrong'
+    };
+    // Duplicate key error
+    if(cusomError.errorCode === 11000) {
+        cusomError.message = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
+        cusomError.status = 400;
+    }
+
+    // Validation error
+    if(err.name === 'ValidationError') {
+        cusomError.message = Object.values(err.errors).map(value => value.message).join(', ');
+        cusomError.status = 400;
+    }
+
+    // Mongoose bad object id
+    if(err.name === 'CastError') {
+        cusomError.message = `No item found with id: ${err.value}`;
+        cusomError.status = 404;
+    }
+
+    // JWT error
+    if(err.name === 'JsonWebTokenError') {
+        cusomError.message = `JWT Error: ${err.message}`;
+        cusomError.status = 401;
+    }
+
+    // JWT expired error
+    if(err.name === 'TokenExpiredError') {
+        cusomError.message = `JWT expired at: ${err.expiredAt}`;
+        cusomError.status = 401;
+    }
+    
+    res.status(cusomError.status).json({
+        error: cusomError.message
     });
 }
 
